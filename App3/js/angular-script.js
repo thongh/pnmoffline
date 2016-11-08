d34f7d1b418f070d408dae7b068e67712c8f82e3
IEUser <IEUser@MSEDGEWIN10>
2016-11-07 23:46:08 -0800

    Add project files.

---
+++ App3/js/angular-script.js
@@ -0,0 +1,58 @@
+﻿// Code goes here
+(function () {
+    var app = angular.module("brazos", ['winjs']);
+
+    app.controller("StoreController", function () {
+        this.products = gems;
+        this.addComment = function (newComment) {
+
+        }
+    });
+
+    app.controller("PanelController", function () {
+        // Controller to handle tabs UI logic
+        // First, create a property 'tab' and initialize it to 1
+        this.tab = 1;
+        // Implement ng-click="tab = 1"
+        this.selectTab = function (settab) {
+            this.tab = settab;
+        };
+        this.isSelected = function (checktab) {
+            return this.tab === checktab;
+        }
+    });
+
+    app.controller("ReviewController", function () {
+        this.review = {};
+        this.addReview = function (product) {
+            product.reviews.push(this.review);
+            this.review = {};
+        }
+    });
+
+    /* ********* DIRECTIVES ********* */
+    // Brazos template header
+    app.directive("brazosHeader", function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'lib/brazosjs-5.0/controls/brazosTemplate/brazosHeader.html'
+        }
+    });
+
+    app.directive("brazosBody", function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'lib/brazosjs-5.0/controls/brazosTemplate/brazosBody.html'
+        }
+    });
+
+    app.directive("brazosFooter", function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'lib/brazosjs-5.0/controls/brazosTemplate/brazosFooter.html'
+        }
+    });
+
+})();
+
+
