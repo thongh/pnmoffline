73cfb81a99d6da7bdeff93c69b1d7f68e05ab4b1
Thong Huynh <thongh@coutureconsulting.com>
2016-11-21 23:29:43 -0800

    2016-11-22 14:30pm

--- App3/js/app.js
+++ App3/js/app.js
@@ -13,46 +13,17 @@
                 });
         });
 
-    app.controller("IndexedDBController", ['$indexedDB', 'idbService', 'offlineService',
-        function ($indexedDB, idbService, offlineService) {
+    /* ********* CONTROLLERS ********* */
+    // Wins list view controller
+    app.controller("IndexedDBController", ['$indexedDB', 'idbService', 'offlineService', 'soapService', '$q',
+        function ($indexedDB, idbService, offlineService, soapService, $q) {
             var OBJECT_STORE_NAME = 'tasks'; 
 
-            offlineService.persistData();
-
+            
              
     }]);
  
 
-    app.controller("WebserviceController", ['$http', function ($scope, $http) {
-        $scope.tasks = [];
-        var webserviceUrl = "http://msedgewin10:8088/mocksoap12PNMUntetheredServicesSoapSoapBinding?WSDL&interface=soap12PNMUntetheredServicesSoapSoapBinding&part=PNMUntetheredServices.wsdl";
-        $http.get(webserviceUrl).success(function(data) {
-            console.log("successfully receive data from webservice");
-            console.log(data);
-        });
-        
-    }]);
-
-    app.controller("PanelController", function () {
-        // Controller to handle tabs UI logic
-        // First, create a property 'tab' and initialize it to 1
-        this.tab = 1;
-        // Implement ng-click="tab = 1"
-        this.selectTab = function (settab) {
-            this.tab = settab;
-        };
-        this.isSelected = function (checktab) {
-            return this.tab === checktab;
-        }
-    });
-
-    app.controller("ReviewController", function () {
-        this.review = {};
-        this.addReview = function (product) {
-            product.reviews.push(this.review);
-            this.review = {};
-        }
-    });
 
     /* ********* DIRECTIVES ********* */
     // Brazos template header
@@ -130,13 +101,16 @@
     });
 
     // Wins list view
-    app.directive("listView", function () {
+    app.directive("listView", ['idbService', 'offlineService', 'soapService', '$q', 
+        function (idbService, offlineService, soapService, $q) {
         return {
             restrict: 'E',
             templateUrl: 'components/winlistview/winlistview.html',
             controller: function () {
                 
-                this.dataArray = [];
+                // Define the data for list view
+                var dataArray = [];
+                /*
                 dataArray = [
                     { title: "Basic banana", text: "Low-fat frozen yogurt", picture: "images/60banana.png" },
                     { title: "Banana blast", text: "Ice cream", picture: "images/60banana.png" },
@@ -147,23 +121,44 @@
                     { title: "Very vanilla", text: "Frozen custard", picture: "images/60vanilla.png" },
                     { title: "Marvelous mint", text: "Gelato", picture: "images/60mint.png" },
                     { title: "Succulent strawberry", text: "Sorbet", picture: "images/60strawberry.png" }
-                ];
-                
+                ];*/
 
-                var itemList = new WinJS.Binding.List(dataArray);
-                // Create a namespace to make the data publicly
-                // accessible. 
-                var publicMembers =
-                    {
-                        itemList: itemList
-                    };
-                WinJS.Namespace.define("DataExample", publicMembers);
-                // Render this UI
-                WinJS.UI.processAll();
+                //offlineService.persistData();
+                var getFieldCheckPermitsByUserPromise = soapService.getFieldCheckPermitsByUser2();
+                var getBulkInstanceDetailsPromise = soapService.getBulkInstanceDetails;
+                var renderListPromise = function (bulkInstanceDetailsReponse) {
+                    var processDetails =  bulkInstanceDetailsReponse.response.data.processDetails;
+                    for (var i = 0; i < processDetails.length; i++) {
+                        dataArray[dataArray.length] = { title: processDetails[i].name, text: processDetails[i].tasks[processDetails[i].tasks.length-1].displayName };
+                    }
+                    var itemList = new WinJS.Binding.List(dataArray);
+                    // Create a namespace to make the data publicly
+                    // accessible. 
+                    var publicMembers =
+                        {
+                            itemList: itemList
+                        };
+                    WinJS.Namespace.define("DataExample", publicMembers);
+                    // Render this UI
+                    WinJS.UI.processAll();
+                };
+
+                // Chain the promises
+                getFieldCheckPermitsByUserPromise.then(getBulkInstanceDetailsPromise,
+                    function (reason) {
+                        Windows.UI.Popups.MessageDialog(reason).showAsync();
+                    }).then(renderListPromise, 
+                    function(reason) {
+                        Windows.UI.Popups.MessageDialog(reason).showAsync();
+                    });
+               
+                
             },
             controllerAs: 'listviewCtrl'
         };
-    });
+     }]);
+
+
 })();
 
 
--- App3/js/webService.js
+++ App3/js/webService.js
@@ -6,7 +6,7 @@
     var app = angular.module("webserviceHelper", []);
 
 
-    app.factory("soapService", ['indexedDB', function ($indexedDB) {
+    app.factory("soapService", ['$q', function ($q) {
         // Soap WSDL url
         // sample: http://host:port/soapserviceendpoint?WSDL
         this.wsdlUrl = "";
@@ -18,6 +18,108 @@
         // Soap response type
         this.soapResType = "xml";
         // Todo: continue defactor ...
+
+        // FUNCTION: call BPM web service to get instances by loginName
+        function asyncGetFieldCheckPermitsByUser() {
+          // perform some asynchronous operation, resolve or reject the promise when appropriate.
+          return $q(function(resolve, reject) {
+              var mockUrl = "http://MSEDGEWIN10:8088/mocksoap12PNMUntetheredServicesSoapSoapBinding$WSDL";
+              var mockUrl2 = "https://demo-machine.corp.coutureconsulting.com:9443/teamworks/webservices/PNMS/PNMUntetheredServices.tws?WSDL";
+              var mockUrl3 = "https://bpm856pc.corp.coutureconsulting.com:9443/teamworks/webservices/PNMS/PNMUntetheredServices.tws?WSDL";
+              var pnmurl = "https://albvbpmdev01.pnmr.internal.corp:9444/teamworks/webservices/PNMS/PNMUntetheredServices.tws?WSDL";
+              var mockSoapReqMsg =
+                  '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:pnm="http://PNMS/PNMUntetheredServices.tws">'
+                   + '<soap:Header/>'
+                   + '<soap:Body>'
+                   + '<pnm:GetFieldCheckPermitsByUser>'
+                   + '<pnm:loginName>thongh</pnm:loginName>'
+                   + '</pnm:GetFieldCheckPermitsByUser>'
+                   + '</soap:Body>'
+                   + '</soap:Envelope>';
+
+              var mockSoapReqMsg2 =
+                  '<Envelope xmlns="http://www.w3.org/2003/05/soap-envelope">'
+                   + '<Body>'
+                   + '<GetFieldCheckPermitsByUser xmlns="http://PNMS/PNMUntetheredServices.tws">'
+                   + '<loginName>thongh</loginName>'
+                   + '</GetFieldCheckPermitsByUser>'
+                   + '</Body>'
+                   + '</Envelope>';
+
+              var options = {
+                  type: "POST",
+                  url: mockUrl3,
+                  responseType: "document",
+                  data: mockSoapReqMsg2
+              };
+              return WinJS.xhr(options)
+              .done(
+              function completed(result) {
+                  console.log("Successfully call SOAP to get permits instance ids by user login");
+                  var $instanceIdNode = angular.element(result.response).find("instanceIDs");
+                  var $items = $instanceIdNode.find("item");
+                  var instanceIDs = [];
+                  for (var i = 0; i < $items.length; i++) {
+                      instanceIDs.push($items[i].innerHTML);
+                  }
+                  // Resolve the promise with the instance ids
+                  console.log(instanceIDs);
+                  resolve(instanceIDs);
+              },
+              function error(error) {
+                  Windows.UI.Popups.MessageDialog(error.status + " : " + error.statusText, "Status").showAsync();
+                  console.log("wsdl call error");
+                  console.log(error);
+                  reject(error);
+              },
+              function progress(progress) {
+                  console.log("wsdl call in progress");
+              });
+          });
+        }
+
+        // FUNCTION: get instance details by instance IDs
+        function asyncGetBulkInstanceDetails(instanceIDs) {
+            // perform some asynchronous operation, resolve or reject the promise when appropriate.
+            var url = "https://bpm856pc.corp.coutureconsulting.com:9443/rest/bpm/wle/v1/process?action=getdetails&instanceIds=";
+            var instanceIDsString = "";
+            for (var i = 0; i < instanceIDs.length; i++) {
+                instanceIDsString = instanceIDsString.concat(instanceIDs[i]).concat(",");
+            }
+            instanceIDsString = encodeURIComponent(instanceIDsString);
+            url = url.concat(instanceIDsString).concat("&parts=all");
+            // Username and password for REST API
+            var username = "deadmin";
+            var password = "teamworks";
+            return $q(function (resolve, reject) {    
+                var options = {
+                    type: "GET",
+                    url: url,
+                    responseType: "json",
+                    headers: {
+                        "Authorization": "Basic " + btoa(username + ":" + password)
+                    },
+                };
+                return WinJS.xhr(options)
+                .done(
+                function completed(result) {
+                    console.log("successfully call BPM REST API with WinJS");
+                    console.log(result);
+                    resolve(result);
+                },
+                function error(error) {
+                    Windows.UI.Popups.MessageDialog(error.status + " : " + error.statusText, "Status").showAsync();
+                    console.log("Error calling REST API to retrieve instance details");
+                    console.log(error);
+                    reject(error);
+                },
+                function progress(progress) {
+                    console.log("wsdl call in progress");
+                    console.log(progress);
+                });
+            });
+
+        }
 
         return {
 
@@ -90,64 +192,9 @@
                 });
             },
     
-            getFieldCheckPermitsByUser2: function () {
-                var mockUrl = "http://MSEDGEWIN10:8088/mocksoap12PNMUntetheredServicesSoapSoapBinding$WSDL";
-                var mockUrl2 = "https://demo-machine.corp.coutureconsulting.com:9443/teamworks/webservices/PNMS/PNMUntetheredServices.tws?WSDL";
-                var mockUrl3 = "https://bpm856pc.corp.coutureconsulting.com:9443/teamworks/webservices/PNMS/PNMUntetheredServices.tws?WSDL";
-                var mockSoapReqMsg =
-                    '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:pnm="http://PNMS/PNMUntetheredServices.tws">'
-                     + '<soap:Header/>'
-                     + '<soap:Body>'
-                     + '<pnm:GetFieldCheckPermitsByUser>'
-                     + '<pnm:loginName>thongh</pnm:loginName>'
-                     + '</pnm:GetFieldCheckPermitsByUser>'
-                     + '</soap:Body>'
-                     + '</soap:Envelope>';
+            getFieldCheckPermitsByUser2: asyncGetFieldCheckPermitsByUser,
+            getBulkInstanceDetails: asyncGetBulkInstanceDetails
 
-                var mockSoapReqMsg2 =
-                    '<Envelope xmlns="http://www.w3.org/2003/05/soap-envelope">'
-                     + '<Body>'
-                     + '<GetFieldCheckPermitsByUser xmlns="http://PNMS/PNMUntetheredServices.tws">'
-                     + '<loginName>thongh</loginName>'
-                     + '</GetFieldCheckPermitsByUser>'
-                     + '</Body>'
-                     + '</Envelope>';
-
-                var options = {
-                    type: "POST",
-                    url: mockUrl3,
-                    responseType: "document",
-                    //headers: {
-                    //    "Content-Type": "text/xml; charset=utf-8",
-                    //    "SOAPAction": "GetFieldCheckPermitsByUser"
-                    //},
-                    data: mockSoapReqMsg2
-                };
-
-                console.log("SOAP message");
-                console.log(options.data);
-
-                return WinJS.xhr(options)
-                .done(
-                function completed(result) {
-                    console.log("successfully call SOAP with WinJS");
-                    console.log(result);
-                    console.log("look for instanceIds");
-                    var $instanceIdNode = angular.element(result.response).find("instanceIDs");
-                    var $items = $instanceIdNode.find("item");
-                    console.log($items);
-                },
-                function error(error) {
-                    Windows.UI.Popups.MessageDialog(error.status + " : " + error.statusText, "Status").showAsync();
-                    console.log("wsdl call error");
-                    console.log(error);
-                },
-                function progress(progress) {
-                    console.log("wsdl call in progress");
-                    console.log(progress);
-                });
-
-            }
         };
     }]);
 
